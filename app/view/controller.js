134d101f9657bfd055613848171e29eec18217a6
Lavan Sri Chandrahasan <LA1Studios@users.noreply.github.com>
2017-07-24 21:45:27 +0530

    no

--- app/view/controller.js
+++ app/view/controller.js
@@ -1,11 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  isCentered: false,
-  actions : {
-    centerMap : function () {
-      alert('triggered')
-      this.trigger('recenter');
-    }
-  }
+
 });
