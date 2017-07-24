5c51ffeb861a04eb05498bb0816ed5dcaf2ac18d
Lavan Sri Chandrahasan <LA1Studios@users.noreply.github.com>
2017-07-24 19:56:31 +0530

    Not perfect

--- app/view/controller.js
+++ app/view/controller.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  isCentered: false,
+  actions : {
+    centerMap : function () {
+      alert('triggered')
+      this.trigger('recenter');
+    }
+  }
 });
--- app/view/route.js
+++ app/view/route.js
@@ -4,5 +4,13 @@ export default Ember.Route.extend({
   model(){
     var members = Ember.$.getJSON("http://localhost:8080/SearchJson.do");
     return members;
-  }
+  },
+  actions: {
+    hello(name) {
+      console.log("Hello "+name);
+    },
+    showModal() {
+      return 1;
+    }
+  },
 });
